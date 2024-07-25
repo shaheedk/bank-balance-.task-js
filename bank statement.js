475b27c6dbcca6d5c491f52d93ce485538ba94b5
Shaheed mon <sheheedk07@gmail.com>
2024-07-25 20:28:18 +0530

    initial commit

---
+++ bank statement.js
@@ -0,0 +1,28 @@
+var balanceAmount=0;
+
+function deposit(){
+    depositAmount = document.getElementById("depos").value
+    depositAmount=Number(depositAmount)
+    if(depositAmount>0){
+        balanceAmount+=depositAmount
+         document.getElementById("depos").value=""
+    }else{
+        alert('enter a valid amound')
+        document.getElementById("depos").value=""
+    }
+}
+function withdraw(){
+    withdrawAmount=document.getElementById("withd").value
+withdrawAmount=Number(withdrawAmount)
+if(withdrawAmount>0 && balanceAmount>=withdrawAmount){
+    balanceAmount-=withdrawAmount
+ document.getElementById("withd").value=""
+}else{
+alert("invalid")
+ document.getElementById("withd").value=""
+}
+
+}
+function  balance(){
+    document.getElementById("bal").innerHTML='$'+balanceAmount
+}
